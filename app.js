c5829e828edc0a4336c4efebba7f209ea10d0dda
doodelidodo <dominic.meyer@me.com>
2020-07-26 10:57:16 +0200

    server structure with data

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+app.use('/static', express.static('public'));
+app.set('view engine', 'pug');
+
+const mainRoutes = require('./routes');
+
+app.use(mainRoutes);
+
+app.use((req, res, next) => {
+    const err = new Error('not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('/error', err);
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost: 3000');
+});
